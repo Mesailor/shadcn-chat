cda5c479a2cfca8457d3c41addcbaf006caeece4
Aleksei Nikolskii <worknikolskii@gmail.com>
2025-12-17 11:11:26 +0400

    fix. minor

--- src/components/common/anchor.tsx
+++ src/components/common/anchor.tsx
@@ -13,7 +13,7 @@ export function Anchor({
 }) {
   return (
     <div
-      className={cn("group flex items-baseline gap-3 scroll-m-16", className)}
+      className={cn("group flex items-baseline gap-3 scroll-m-18", className)}
       id={id}
     >
       {children}
