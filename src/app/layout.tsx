efb437b710c238aebb8b5c83c1fd04a5821965ae
Aleksei Nikolskii <worknikolskii@gmail.com>
2026-01-22 11:34:07 +0400

    feat: add google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 export const metadata: Metadata = {
   title: "Shadcn Chat Component",
   description: "A customizable chat component built with shadcn/ui and React.",
+  verification: {
+    google: "WAtEv2A2DYHObM27Q3_lWjpwivMZEFz0-EwAopO2Jlw",
+  },
 };
 
 export default function RootLayout({
