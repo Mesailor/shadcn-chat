51f2f9d375bac1ab344b77f83c8a848470b3b113
Aleksei Nikolskii <worknikolskii@gmail.com>
2026-01-26 11:11:18 +0400

    fix: use correct base url in production + no robots dev

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,17 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const isNotProduction = process.env.VERCEL_ENV !== "production";
+
+  if (isNotProduction) {
+    return {
+      rules: {
+        userAgent: "*",
+        disallow: "/",
+      },
+    };
+  }
 
   return {
     rules: {
