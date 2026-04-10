800584bc9e0cf0050fdb65cf7fc89451dee264ae
Aleksei Nikolskii <worknikolskii@gmail.com>
2026-04-10 11:30:52 +0400

    refactor: rename message examples dir

--- src/components/examples/chat-example-component.tsx
+++ src/components/examples/chat-example-component.tsx
@@ -72,12 +72,12 @@ import {
 } from "@/components/ui/dialog";
 import EmojiPicker, { EmojiClickData, Theme } from "emoji-picker-react";
 import { ChatMessages } from "@/registry/new-york/chat/chat-messages";
-import { PrimaryMessage } from "@/components/examples/messages/primary-message";
-import { MessagePreview } from "@/components/examples/messages/message-preview";
-import { DateItem } from "@/components/examples/messages/date-item";
-import { AdditionalMessage } from "@/components/examples/messages/additional-message";
-import { PrimaryMessageSkeleton } from "@/components/examples/messages/primary-message-skeleton";
-import { DateItemSkeleton } from "@/components/examples/messages/date-item-skeleton";
+import { PrimaryMessage } from "@/components/examples/message-items/primary-message";
+import { MessagePreview } from "@/components/examples/message-items/message-preview";
+import { DateItem } from "@/components/examples/message-items/date-item";
+import { AdditionalMessage } from "@/components/examples/message-items/additional-message";
+import { PrimaryMessageSkeleton } from "@/components/examples/message-items/primary-message-skeleton";
+import { DateItemSkeleton } from "@/components/examples/message-items/date-item-skeleton";
 import { useCallback, useEffect, useRef, useState } from "react";
 import {
   CURRENT_USER,
--- src/components/page-sections/usage-example/event-usage-example.tsx
+++ src/components/page-sections/usage-example/event-usage-example.tsx
@@ -9,7 +9,7 @@ import {
   ChatEventTime,
   ChatEventTitle,
 } from "@/registry/new-york/chat/chat-event";
-import { DateItem } from "@/components/examples/messages/date-item";
+import { DateItem } from "@/components/examples/message-items/date-item";
 import { Anchor } from "@/components/common/anchor";
 import { HighlightedComponent } from "@/components/ui/typography";
 
