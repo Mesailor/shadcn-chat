87a642baa1a1f8ae31a459e34ddabaaedbab55b4
Aleksei Nikolskii <worknikolskii@gmail.com>
2026-06-08 15:54:01 +0400

    feat: add logo and max-width to the header

--- src/components/page-sections/app-header.tsx
+++ src/components/page-sections/app-header.tsx
@@ -1,43 +1,54 @@
 import { Button } from "@/components/ui/button";
 import { MessageCircleIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
 import { ThemeToggle } from "./theme-toggle";
 
 export function AppHeader() {
   return (
     <header className="px-4 py-3 fixed top-0 left-0 w-full backdrop-blur-lg border-b z-50">
-      <div className="ml-auto w-fit flex items-center gap-2">
-        <Button
-          size="sm"
-          className="min-w-20 bg-linear-to-br from-yellow-700 to-amber-500 hover:opacity-90 text-white"
-          asChild
-        >
-          <a
-            href="https://github.com/Mesailor/shadcn-chat"
-            target="_blank"
-            rel="noreferrer"
+      <div className="mx-auto max-w-7xl flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={32}
+            height={32}
+            className="rounded-sm"
+          />
+          <span className="font-semibold">Chat Component</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button
+            className="size-9 md:size-auto md:min-w-20 bg-linear-to-br from-yellow-700 to-amber-500 hover:opacity-90 text-white"
+            asChild
           >
-            <StarIcon />
-            Star on GitHub
-          </a>
-        </Button>
+            <a
+              href="https://github.com/Mesailor/shadcn-chat"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StarIcon />
+              <span className="hidden md:inline">Star on GitHub</span>
+            </a>
+          </Button>
 
-        <Button
-          className="dark:bg-background dark:hover:bg-accent"
-          size="sm"
-          variant="outline"
-          asChild
-        >
-          <a
-            href="https://github.com/Mesailor/shadcn-chat/issues"
-            target="_blank"
-            rel="noreferrer"
+          <Button
+            className="size-9 md:size-auto dark:bg-background dark:hover:bg-accent"
+            variant="outline"
+            asChild
           >
-            <MessageCircleIcon />
-            Feedback
-          </a>
-        </Button>
+            <a
+              href="https://github.com/Mesailor/shadcn-chat/issues"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MessageCircleIcon />
+              <span className="hidden md:inline">Feedback</span>
+            </a>
+          </Button>
 
-        <ThemeToggle />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
