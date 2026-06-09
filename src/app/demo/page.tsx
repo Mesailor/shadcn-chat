35eb72b8b123ba0f969909def2b8dee47b87f514
Aleksei Nikolskii <worknikolskii@gmail.com>
2026-06-09 13:51:58 +0400

    feat(full-screen-page): add a page with full-screen chat demo

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,9 @@
+import { ChatExampleComponent } from "@/components/examples/chat-example-component";
+
+export default function DemoPage() {
+  return (
+    <div className="h-svh overflow-hidden">
+      <ChatExampleComponent />
+    </div>
+  );
+}
--- src/components/page-sections/component-demo-section.tsx
+++ src/components/page-sections/component-demo-section.tsx
@@ -5,11 +5,13 @@ import { CSSProperties, ReactNode, useRef, useState } from "react";
 import { ButtonGroup } from "@/components/ui/button-group";
 import { Button } from "@/components/ui/button";
 import {
+  Maximize2Icon,
   MessageCircleIcon,
   MonitorIcon,
   SmartphoneIcon,
   TabletIcon,
 } from "lucide-react";
+import Link from "next/link";
 
 const SCREEN_SIZES: { [key: string]: CSSProperties } = {
   desktop: { width: "100%", maxWidth: 1280, height: "85vh" },
@@ -41,18 +43,26 @@ export function ComponentDemoSection() {
       id="demo"
       className="min-h-[600px] w-full py-8 md:px-4 space-y-4 flex flex-col items-center scroll-m-12"
     >
-      <ButtonGroup>
-        {BUTTONS.map((button) => (
-          <Button
-            key={button.size}
-            size="icon-sm"
-            variant={screenSize === button.size ? "default" : "outline"}
-            onClick={() => handleSetScreenSize(button.size)}
-          >
-            {button.icon}
-          </Button>
-        ))}
-      </ButtonGroup>
+      <div className="flex items-center gap-2">
+        <ButtonGroup className="hidden sm:flex">
+          {BUTTONS.map((button) => (
+            <Button
+              key={button.size}
+              size="icon-sm"
+              variant={screenSize === button.size ? "default" : "outline"}
+              onClick={() => handleSetScreenSize(button.size)}
+            >
+              {button.icon}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <Button className="sm:size-8" variant="outline" asChild>
+          <Link href="/demo">
+            <span className="sm:hidden">Full-screen demo</span>
+            <Maximize2Icon />
+          </Link>
+        </Button>
+      </div>
       <div
         className="border rounded-lg overflow-hidden transition-all duration-500 max-h-[1080px]"
         style={{
